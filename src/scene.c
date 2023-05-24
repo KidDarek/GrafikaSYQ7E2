5271dc0e892b6d84672c423da793d9c087c441e6
KidDarek <madanotsirk@gmail.com>
2023-05-24 08:29:31 +0200

    i

--- src/scene.c
+++ src/scene.c
@@ -9,32 +9,37 @@ GLfloat matrix[16];
 
 void init_scene(Scene *scene)
 {
-    int row = -2;
+    int row = 1;
     scene->models = malloc(sizeof(ModelData) * 33);
     for (int i = 0; i < 16; i++)
     {
         if (i == 8)
         {
-            row += 2;
+            row += 1.2;
         }
         load_model(&(scene->models[i].model), "assets/models/paraszt.obj");
         scene->models[i].texture_id = load_texture("assets/textures/Brick.jpg");
         glBindTexture(GL_TEXTURE_2D, scene->models[i].texture_id);
-        init_vec3(&(scene->models[i].positon), row, (i % 8) * 2, 0);
+        init_vec3(&(scene->models[i].positon), row, (i % 8) + 0.4, 0);
         init_vec3(&(scene->models[i].rotation), 0, 0, 0);
     }
     for (int i = 16; i < 32; i++)
     {
         if (i == 24)
         {
-            row += 2;
+            row += 1;
         }
         load_model(&(scene->models[i].model), "assets/models/paraszt.obj");
         scene->models[i].texture_id = load_texture("assets/textures/Brick.jpg");
         glBindTexture(GL_TEXTURE_2D, scene->models[i].texture_id);
-        init_vec3(&(scene->models[i].positon), row + 12, (i % 8) * 2, 0);
+        init_vec3(&(scene->models[i].positon), row + 4.7, (i % 8) + 0.4, 0);
         init_vec3(&(scene->models[i].rotation), 0, 0, 180);
     }
+    load_model(&(scene->models[32].model), "assets/models/table.obj");
+    scene->models[32].texture_id = load_texture("assets/textures/BoardColor.jpg");
+    glBindTexture(GL_TEXTURE_2D, scene->models[32].texture_id);
+    init_vec3(&(scene->models[32].positon), 4.5, 4.5, -0.6);
+    init_vec3(&(scene->models[32].rotation), 0, 0, 0);
 
     scene->material.ambient.red = 0.0;
     scene->material.ambient.green = 0.0;
@@ -102,7 +107,7 @@ void render_scene(const Scene *scene)
     set_material(&(scene->material));
     set_lighting(0);
     draw_origin();
-    for (int i = 0; i < 33; i++)
+    for (int i = 0; i < 32; i++)
     {
         glLoadMatrixf(matrix);
         glTranslatef(scene->models[i].positon.x, scene->models[i].positon.y, scene->models[i].positon.z);
@@ -111,6 +116,12 @@ void render_scene(const Scene *scene)
         glRotatef(scene->models[i].rotation.z, 0.0, 0.0, 1.0);
         draw_model(&(scene->models[i].model));
     }
+    glLoadMatrixf(matrix);
+    glTranslatef(scene->models[32].positon.x, scene->models[32].positon.y, scene->models[32].positon.z);
+    glRotatef(scene->models[32].rotation.x, 1.0, 0.0, 0.0);
+    glRotatef(scene->models[32].rotation.y, 0.0, 1.0, 0.0);
+    glRotatef(scene->models[32].rotation.z, 0.0, 0.0, 1.0);
+    draw_model(&(scene->models[32].model));
     glLoadMatrixf(matrix);
 }
 
